209b4c47dab896d7d4bc397db4e0ad1de78cf412
Kemon1 <kamilbeluch123@gmail.com>
2024-11-13 15:48:13 +0000

    added user registration & crispy forms

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
+from users import views as p_views
 
 app_name = 'itreporting'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name = 'about'),
-    path('contact', views.contact, name = 'contact')
+    path('contact', views.contact, name = 'contact'),
+    path('register', p_views.register, name = 'register'),
 ]
