5dd14014dc1f83deef1d2b7619e1ad761092fcdc
Kemon1 <kamilbeluch123@gmail.com>
2024-11-14 10:23:50 +0000

    added login & logout with restrictions

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name = 'about'),
     path('contact', views.contact, name = 'contact'),
-    path('register', p_views.register, name = 'register'),
+    path('register', p_views.register, name = 'register'), 
 ]
