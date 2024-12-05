1bbff1bf8fb07f6fed63032d2ba4a10d4c0e44a7
Kemon1 <kamilbeluch123@gmail.com>
2024-12-05 12:59:09 +0000

    with updated hsot info

--- itapps/settings.py
+++ itapps/settings.py
@@ -9,18 +9,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your_current_secret_key_value') 
 
-WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', None)
+WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', None) 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = WEBSITE_HOSTNAME == None
+DEBUG = WEBSITE_HOSTNAME == None 
 
-if DEBUG:
-    ALLOWED_HOSTS = []
-else:
-    ALLOWED_HOSTS = [WEBSITE_HOSTNAME]
-    CSRF_TRUSTED_ORIGINS = [f'https://{WEBSITE_HOSTNAME}']
+if DEBUG: 
+
+    ALLOWED_HOSTS = [] 
+
+else: 
+
+    ALLOWED_HOSTS = [WEBSITE_HOSTNAME] 
+
+    CSRF_TRUSTED_ORIGINS = [f'https://{WEBSITE_HOSTNAME}'] 
+
 
 
 # Application definition
