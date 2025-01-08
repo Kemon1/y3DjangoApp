e5e5158ded46e1a0c7c201e491e57e04581d7f1e
Kemon1 <kamilbeluch123@gmail.com>
2025-01-08 16:31:46 +0000

    added password reset function

--- itapps/urls.py
+++ itapps/urls.py
@@ -17,7 +17,11 @@ urlpatterns = [
     path('course', courses_views.my_course, name='my_course'),
     path('modules/', ModuleListView.as_view(template_name='modules/module_list.html'), name = 'modules'),
     path('module/<int:pk>/', ModuleDetailView.as_view(template_name='modules/module_detail.html'), name = 'module-detail'),
-    path('enroll/<int:module_id>/', enroll_in_module, name = 'enroll-in-module')
+    path('enroll/<int:module_id>/', enroll_in_module, name = 'enroll-in-module'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name ='password_reset'),
+    path('password_reset/done', auth_views.PasswordChangeDoneView.as_view(template_name = 'users/password_reset_done.html'), name ='password_reset_done'),
+    path('reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),  name = "password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name = "password_reset_complete"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
